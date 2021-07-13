eb59047340f9786cd567231bab81f2aca72d1eb9
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-07-12 21:11:26 -0500

    operadores asignacion

---
+++ main.js
@@ -0,0 +1,44 @@
+
+let x = 6;
+let y = 9;
+
+//Operador de exponenciacion
+// x **= y;
+// x = x ** y;
+// console.log(x);
+
+
+// Operador de Resto
+// x %= y;
+// x = x % y;
+// console.log(x);
+
+
+// Operador de Division
+// x /= y;
+// x = x / y;
+// console.log(x);
+
+
+// Operador de Multiplicacion
+// x *= y;
+// x = x * y;
+// console.log(x);
+
+
+// Operador de resta
+// x -= y;
+// x = x - y;
+// console.log(x);
+
+
+
+// Operador de suma
+// x += y;
+// x = x + y;
+// console.log(x);
+
+
+// Operador de asignación
+// x = y;
+// console.log(x);
